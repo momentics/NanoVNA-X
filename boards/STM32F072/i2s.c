48a53edad887c156d572e8fe877d92e7658fa914
user <user@example.com>
2025-12-17 04:20:07 +0300

    remove I2S clock polarity inversion and add TLV320AIC3204 drive zero for unused bits.

--- boards/STM32F072/i2s.c
+++ boards/STM32F072/i2s.c
@@ -59,7 +59,6 @@ void init_i2s(void *buffer, uint16_t count) {
 //  | SPI_I2SCFGR_I2SCFG_1        //
     | SPI_I2SCFGR_I2SMOD          // I2S mode is selected
     | SPI_I2S_MSB_MODE            // MSB Justified (LJF)
-    | SPI_I2SCFGR_CKPOL           // Invert clock polarity (sample on falling edge)
     | SPI_I2SCFGR_I2SE            // I2S enable
     ;
 }
--- src/platform/peripherals/tlv320aic3204.c
+++ src/platform/peripherals/tlv320aic3204.c
@@ -122,9 +122,10 @@
 #define REG_27_INTERFACE_DSP (1 << 6)
 #define REG_27_INTERFACE_RJF (2 << 6)
 #define REG_27_INTERFACE_LJF (3 << 6)
+#define REG_27_DRIVE_ZERO (1 << 0)
 
-// Set the interface mode: 16 bit, LJF mode, BCLK & WCLK as INPUT (High-Z) initially
-#define REG_27 (REG_27_DATA_16 | REG_27_INTERFACE_LJF | REG_27_WCLK_IN | REG_27_BCLK_IN)
+// Set the interface mode: 16 bit, LJF mode, BCLK & WCLK as INPUT (High-Z) initially, Drive 0 on unused bits
+#define REG_27 (REG_27_DATA_16 | REG_27_INTERFACE_LJF | REG_27_WCLK_IN | REG_27_BCLK_IN | REG_27_DRIVE_ZERO)
 #define REG_30(n) (0x80 + ((n) * sizeof(int16_t) / sizeof(audio_sample_t)))
 
 static const uint8_t conf_data[] = {
@@ -510,5 +511,5 @@ void tlv320aic3204_set_gain(uint8_t lgain, uint8_t rgain) {
 void tlv320aic3204_start_clocks(void) {
   // Set the interface mode: 16 bit, LJF mode, BCLK & WCLK as OUTPUT
   // This is called after I2S is enabled to ensure synchronization
-  tlv320aic3204_write_reg(0, 27, REG_27_DATA_16 | REG_27_INTERFACE_LJF | REG_27_WCLK_OUT | REG_27_BCLK_OUT);
+  tlv320aic3204_write_reg(0, 27, REG_27_DATA_16 | REG_27_INTERFACE_LJF | REG_27_WCLK_OUT | REG_27_BCLK_OUT | REG_27_DRIVE_ZERO);
 }
