0ca92d6bf80727601f897304951badac921005ea
momentics <momentics@gmail.com>
2025-12-21 01:50:09 +0300

    Increase SERIAL_USB_TX_BUFFERS_NUMBER from 2 to 4

--- halconf.h
+++ halconf.h
@@ -333,7 +333,7 @@
 #define SERIAL_USB_RX_BUFFERS_NUMBER 1
 #endif
 #if !defined(SERIAL_TX_USB_BUFFERS_NUMBER) || defined(__DOXYGEN__)
-#define SERIAL_USB_TX_BUFFERS_NUMBER 2
+#define SERIAL_USB_TX_BUFFERS_NUMBER 4
 #endif
 
 // USB Send timeout
